e7e4e86a056810eaeb45542d149dfc46bbcb245f
Luxonline <105938503+DragonlordNewb@users.noreply.github.com>
2022-11-18 15:13:17 +0000

    commit

--- clearsight_2/neocortex/cores/dataacquisition.py
+++ clearsight_2/neocortex/cores/dataacquisition.py
@@ -1,5 +1,6 @@
 from clearsight_2.neocortex import memory
+from clearsight_2.neocortex.languageprocessing import tokenization
 
 def feed(string):
     memory.commit("MAIN", string)
-    return "..."
+    return str(tokenization.tokenize(string))
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.py
     pkgs.unixtools.arp
     pkgs.python38Full
   ];
