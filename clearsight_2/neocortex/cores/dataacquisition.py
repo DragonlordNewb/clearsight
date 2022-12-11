72c53efe10f11d141a0d1e27dc61556d6ed0726e
ke-1 <jacobbodell@gmail.com>
2022-12-11 10:19:54 -0600

    commit

--- clearsight_2/neocortex/cores/dataacquisition.py
+++ clearsight_2/neocortex/cores/dataacquisition.py
@@ -2,8 +2,8 @@ from clearsight_2.neocortex import memory
 from clearsight_2.neocortex.languageprocessing import tokenization
 
 def feed(string):
-    memory.commit("MAIN", string)
+    # memory.commit("MAIN", string)
     tok = tokenization.tokenize(string)
-    for tobject in [x[0] for x in tok.objects]:
+    for tobject in [x[0] for x in tok.properNouns]:
         if not memory.memoryFileExists(tobject[0]):
             return "What is " + str(tobject[0])
